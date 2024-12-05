78506d8305859f6edc2e732303de87482829f708
Claudia Tudor <claudiaioana20983@gmail.com>
2024-12-06 01:10:29 +0200

    Update and rename S3_Problem5_TCI.py to minutes.py

--- S3_Problem5_TCI.py
+++
@@ -1,8 +0,0 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-
-print("Please enter the number of minutes:")
-minutes_tci=int(input())
-hours_tci=minutes_tci//60
-minutes_remaining=minutes_tci%60
-print(minutes_tci , "minutes represent" , hours_tci , "hours and" , minutes_remaining , "minutes")
---
+++ minutes.py
@@ -0,0 +1,5 @@
+print("Please enter the number of minutes:")
+minutes_tci=int(input())
+hours_tci=minutes_tci//60
+minutes_remaining=minutes_tci%60
+print(minutes_tci , "minutes represent" , hours_tci , "hours and" , minutes_remaining , "minutes")
