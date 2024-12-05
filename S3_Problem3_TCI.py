1d8ef2d2a401e8c7eaaaf91ca4387548cfd9464e
Claudia Tudor <claudiaioana20983@gmail.com>
2024-12-06 01:07:43 +0200

    Update S3_Problem3_TCI.py

--- S3_Problem3_TCI.py
+++ S3_Problem3_TCI.py
@@ -1,6 +1,3 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-
 print("Please enter temperature in Fahrenheit degrees:")
 fahrenheit_tci=float(input())
 celsius_tci=((fahrenheit_tci-32)*5)/9
